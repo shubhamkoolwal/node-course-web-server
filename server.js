b0256278cc627d69cbe6d476bda0c4d63388a6f4
Shubham.Koolwal <Shubham.Koolwal@technoforte.co.in>
2017-07-30 23:48:36 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+
+
+//view is the default template directory that why we create views directory and put .hbs file there
+
+
+const express=require('express');
+
+var app=express();
+
+app.use((request,response,next) =>{
+	 next();
+})
+const hbs=require('hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+//key-value pair
+app.set('view engine','hbs');
+// request tore the client information  
+app.get('/',(request,response) =>{
+
+		// response.send('<h1>hello express<h1>');
+
+		response.send(
+
+		{
+			name:'Shubham',
+			likes:['biking','cities']
+		}
+			)
+
+});
+
+app.get('/about',(request,response) =>
+{
+	//response.send('About page');
+	response.render('about.hbs',{
+
+		pagetitle:'about page',
+		currentyear: new Date().getFullYear()
+	})
+});
+
+
+app.get('/bad',(request,response) =>
+{
+
+	response.send(
+			{
+				errormessage:'error !!!!!!!!!!!'
+			}
+		)
+})
+
+
+app.use(express.static(__dirname+'/public'));
+
+
+//bind the request on the specific port
+app.listen(3000,() =>
+	{
+		console.log("server start at port 3000");
+	});
